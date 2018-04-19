b38e373c59d02596460961ce32c9e2539341364e
Kyle Russell <karussel@deakin.edu.au>
2018-04-20 00:48:14 +1000

    Animation Completed
    Animation finished

--- Assets/Scripts/TileMovementController.cs
+++ Assets/Scripts/TileMovementController.cs
@@ -9,48 +9,59 @@ public class TileMovementController : MonoBehaviour {
 	Vector3 pos;                                // For movement
 	float speed = 5.0f;                         // Speed of movement
 
+
+	private bool AnimationLeft;
+	private bool AnimationRight;
+	private bool AnimationUp;
+	private bool AnimationDown;
+
 	void Start () {
 		
 		animRef = GetComponent<Animator> (); // Reference the animation conditions to this class
-
 		pos = transform.position;          // Take the initial position
 	}
 
 	void FixedUpdate ()
 	{
+		
+		AnimationRight = false;
+		AnimationUp = false;
+		AnimationDown = false;
+
+
 		if (Input.GetKey (KeyCode.A) && transform.position == pos) {        // Left
 			pos += Vector3.left;
-
-			while (Vector3.left.x < 0) {
-				animRef.SetBool("AnimationLeft", true);										// activate left animation bool
+			AnimationLeft = true;			// activate left animation bool
 			}
+			else
+		{
+			AnimationLeft = false;
 		}
+
 
 		if (Input.GetKey (KeyCode.D) && transform.position == pos) {        // Right
 			pos += Vector3.right;
-
-			while (Vector3.right.x > 0) {
-				animRef.SetBool("AnimationRight", true);										// activate right animation bool
+			AnimationRight = true;			// activate right animation bool
 			}
-		}
 
 		if (Input.GetKey (KeyCode.W) && transform.position == pos) {        // Up
 			pos += Vector3.up;
-
-			while (Vector3.up.y > 0) {
-				animRef.SetBool("AnimationUp", true);										// activate up animation bool
+			AnimationUp = true;			// activate up animation bool
 			}
-		}
 
-			if (Input.GetKey (KeyCode.S) && transform.position == pos) {        // Down
-				pos += Vector3.down;
-
-			while (Vector3.down.y < 0) {
-					animRef.SetBool("AnimationDown", true);									// activate down animation bool
-				}
-
-
+		if (Input.GetKey (KeyCode.S) && transform.position == pos) {        // Down
+			pos += Vector3.down;
+			AnimationDown = true;			// activate down animation bool
 			}
+
+		animRef.SetBool ("AnimationLeft", AnimationLeft);
+		animRef.SetBool ("AnimationRight", AnimationRight);
+		animRef.SetBool ("AnimationUp", AnimationUp);
+		animRef.SetBool ("AnimationDown", AnimationDown);
+
+		animRef.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		animRef.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+
 		transform.position = Vector3.MoveTowards (transform.position, pos, Time.deltaTime * speed);    // Move there
 		}
 	}
