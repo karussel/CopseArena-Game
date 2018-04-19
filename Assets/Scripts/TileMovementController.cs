877fb9e466bf92d2de841d402ee8a905b4e5816f
Kyle Russell <karussel@deakin.edu.au>
2018-04-19 22:46:20 +1000

    Update
    Animation Update

--- Assets/Scripts/TileMovementController.cs
+++ Assets/Scripts/TileMovementController.cs
@@ -9,13 +9,6 @@ public class TileMovementController : MonoBehaviour {
 	Vector3 pos;                                // For movement
 	float speed = 5.0f;                         // Speed of movement
 
-	public bool animationLeft;					// For activating the left walk animation
-	public bool animationRight;					// For activating the right walk animation
-	public bool animationUp;					// For activating the up walk animation
-	public bool animationDown;					// For activating the down walk animation
-
-
-
 	void Start () {
 		
 		animRef = GetComponent<Animator> (); // Reference the animation conditions to this class
@@ -28,32 +21,32 @@ public class TileMovementController : MonoBehaviour {
 		if (Input.GetKey (KeyCode.A) && transform.position == pos) {        // Left
 			pos += Vector3.left;
 
-			while (Vector3.left.x > 0) {
-				animationLeft = true; animRef.SetBool("AnimationLeft", true);										// activate left animation bool
+			while (Vector3.left.x < 0) {
+				animRef.SetBool("AnimationLeft", true);										// activate left animation bool
 			}
 		}
 
 		if (Input.GetKey (KeyCode.D) && transform.position == pos) {        // Right
 			pos += Vector3.right;
 
-			while (Vector3.left.x < 0) {
-				animationRight = true; animRef.SetBool("AnimationRight", true);										// activate right animation bool
+			while (Vector3.right.x > 0) {
+				animRef.SetBool("AnimationRight", true);										// activate right animation bool
 			}
 		}
 
 		if (Input.GetKey (KeyCode.W) && transform.position == pos) {        // Up
 			pos += Vector3.up;
 
-			while (Vector3.left.y > 0) {
-				animationUp = true; animRef.SetBool("AnimationUp", true);										// activate up animation bool
+			while (Vector3.up.y > 0) {
+				animRef.SetBool("AnimationUp", true);										// activate up animation bool
 			}
 		}
 
 			if (Input.GetKey (KeyCode.S) && transform.position == pos) {        // Down
 				pos += Vector3.down;
 
-				while (Vector3.left.y < 0) {
-				animationDown = true; animRef.SetBool("AnimationDown", true);									// activate down animation bool
+			while (Vector3.down.y < 0) {
+					animRef.SetBool("AnimationDown", true);									// activate down animation bool
 				}
 
 
