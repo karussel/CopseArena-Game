a931664417a02480f54730ca2a0ca2f6af2b2a40
Kyle Russell <karussel@deakin.edu.au>
2018-04-22 12:59:53 +1000

    changes
    Added arena background and moved files to make more sesne

--- Tilesets/roguelikeSheet_transparent.tsx
+++ Tilesets/roguelikeSheet_transparent.tsx
@@ -1,4 +1,49 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="roguelikeSheet_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="1767" columns="57">
  <image source="../Copse Arena/Assets/Art/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="128">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="15.75" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="129">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="-0.25" width="15.5" height="16.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="939">
+  <objectgroup draworder="index">
+   <object id="6" x="0" y="0" width="16" height="15.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1050">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.5" width="15.5" height="14.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1051">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="0" width="16.25" height="15.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="1103">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.25" y="-0.5" width="16.5" height="16.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="1160">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0" width="15.75" height="16.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="1161">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.25" width="15.75" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
