4e0db232f1836b19d138275039b62b91ffd84e1c
Kyle Russell <karussel@deakin.edu.au>
2018-04-29 16:18:39 +1000

    Battle scene
    Added the battle scene and started scripts for the enemy and player

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+
+
+// Use this for initialization
+void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("space"))
+        {
+
+            GetComponent<Animator>().SetTrigger("EnemyMelee"); // trigger the melee animation
+            GetComponent<Transform>().position = new Vector2(2.8f, -0.42f); // Move to enemy position (temporary position move when final asset done)
+        }
+
+    }
+
+    public void returnEnemy()
+    {
+
+        GetComponent<Transform>().position = new Vector2(2.8f, -0.42f);
+
+    }
+
+
+}
